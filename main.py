5bb30e758544786ac20788c56dd34bf7a451ee72
emonahmed123 <imonshomon@gmail.com>
2026-03-26 14:40:00 +0600

    learn variables

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-print("Hello, World!")
-print("Welcome to the world of programming!")
+# print("Hello, World!")
+# print("Welcome to the world of programming!")
 
 
-print(44+10)
+# print(44+10)
 
 
-// Python uses indentation to define blocks of code. Make sure to use consistent indentation (e.g., 4 spaces) for your code.
+#  Python uses indentation to define blocks of code. Make sure to use consistent indentation (e.g., 4 spaces) for your code.
 
+
+ 
+#  learn variables 
+
+# A variable name must start with a letter or an underscore (_) and can be followed by letters, digits, or underscores. Variable names are case-sensitive.don't start with a number and don't use special characters except for underscores.
+
+age = 25
+print(age)
+name = "Alice"
+print(name)
+_score = 90
+print(_score)
