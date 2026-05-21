1bd277499b06bf71e9474dd250a62093170697bf
emonahmed123 <imonshomon@gmail.com>
2026-05-21 09:24:51 +0600

    done

---
+++ REAME.md
@@ -0,0 +1,10 @@
+# Python Learning
+
+In this project, I practiced Python from basic to advanced topics.
+
+## What I Learned
+
+- Python fundamentals
+- Intermediate Python concepts
+- Advanced Python concepts
+- Object-oriented programming (OOP) in Python
--- main.py
+++ main.py
@@ -13,9 +13,40 @@
 
 # A variable name must start with a letter or an underscore (_) and can be followed by letters, digits, or underscores. Variable names are case-sensitive.don't start with a number and don't use special characters except for underscores.
 
-age = 25
-print(age)
-name = "Alice"
-print(name)
-_score = 90
-print(_score)
+# age = 25
+# print(age)
+# name = "Alice"
+# print(name)
+# _score = 90
+# print(_score)
+
+# print(id(age))  # This will give you the memory address of the variable 'age'
+ 
+# con =True
+
+# if con:
+#     print("Condition is True")
+
+# else:
+#     print("Condition is False")
+
+
+"""
+This  type  under Numeric data type
+1. int (integer) - represents whole numbers without a decimal point. Example: 5, -3, 0
+2. float (floating-point) - represents numbers with a decimal point. Example: 3.14, -0.5, 2.0
+3. complex - represents complex numbers with a real and imaginary part. Example: 2 + 3j, -1 - 4j
+"""
+age = 20  #initzer data type
+float_num = 3.14 #float data type
+complex_num = 2 + 3j #complex data type
+
+
+name = "Alice"  #string data type
+
+
+
+# Sequence data types
+# 1. list - an ordered, mutable collection of items. Example: [1, 2, 3], ['a', 'b', 'c']
+
+city= ["New York", "Los Angeles", "Chicago"]  #list data type
