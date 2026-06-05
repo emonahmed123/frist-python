c930d9411e5cf60f696eb728165b257b93a1457e
emonahmed123 <imonshomon@gmail.com>
2026-06-05 19:45:53 +0600

    done

--- condition.py
+++ condition.py
@@ -1,13 +1,25 @@
-a = 10
+# a = 10
 
-if a < 5:
-    print("Condition is True")
+# if a < 5:
+#     print("Condition is True")
 
-elif a == 10:
-    print("Condition is 10")
-else:
-    print("Condition is False")
+# elif a == 10:
+#     print("Condition is 10")
+# else:
+#     print("Condition is False")
 
     
+
+# if a != 10:
+#     print("Condition is not 10")
+
+# else:
+#     print("Condition is 10")
+
+
+a = [1, 2, 3, 4, 5]  # list data type
+if len(a) > 3:
+    print("Condition is True")
+
 
 
