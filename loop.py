10f75c341e32a9dbe1045ac33efe5f6dc8905610
emonahmed123 <imonshomon@gmail.com>
2026-06-09 15:51:33 +0600

    Refactor loop.py to comment out unused code and maintain focus on even number printing

--- loop.py
+++ loop.py
@@ -1,5 +1,25 @@
  
-list1 = [1, 2, 3, 4, 5]
+# list1 = [1, 2, 3, 4, 5]
 
-for item in list1:
-    print(item)
+# for item in list1:
+#     print(item)
+
+
+# i=0
+
+# while  len(list1) <= 5:
+#    print(list1[i])
+
+# b=0 
+# while b<100:
+#     b+=1
+#     print(b)
+
+a =0 
+
+
+while a<100:
+    a+=1
+ 
+    if a%2==0:
+        print("even")
